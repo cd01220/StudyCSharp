588bb8107d492e2306d79e673f0963ac09a0249a
Howard Liu <10861479@qq.com>
2018-04-18 23:38:39 +0800

    study json.net

--- Program.cs
+++ Program.cs
@@ -2,9 +2,12 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+
+    using Newtonsoft.Json;
 
     /// <summary>
     /// the Program class.
@@ -17,6 +20,24 @@
         /// <param name="args">the args</param>
         private static void Main(string[] args)
         {
+            TestJson01();
         }
-    }
+
+        /// <summary>
+        /// to test JsonConvert class.
+        /// For simple scenarios where you want to convert to and from a JSON string, the SerializeObject() 
+        /// and DeserializeObject() methods on JsonConvert provide an easy-to-use wrapper over JsonSerializer.
+        /// </summary>
+        private static void TestJson01()
+        {
+            Student originalObject = new Student();
+            originalObject.ID = 1;
+            originalObject.Name = "Liu Hao";
+
+            string jsonString = JsonConvert.SerializeObject(originalObject);
+            Console.WriteLine(jsonString);
+
+            Student deserializedObject = JsonConvert.DeserializeObject(jsonString, typeof(Student)) as Student;
+        }
+    }    
 }
---
+++ Student.cs
@@ -0,0 +1,24 @@
+﻿namespace StudyCSharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// the entity class Student.
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// Gets or sets the ID of the student.
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Name of the student.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
