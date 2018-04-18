b5004e888fdb206ff1486bc7c1109ea8c992aa94
Howard Liu <10861479@qq.com>
2018-04-18 22:33:45 +0800

    update code style in according to StyleCop.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StudyCSharp
+﻿namespace StudyCSharp
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// the Program class.
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// the Main method.
+        /// </summary>
+        /// <param name="args">the args</param>
+        private static void Main(string[] args)
         {
         }
     }
