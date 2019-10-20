c398f5d3bbf75b1755a53a713fad8f6d13f4574d
Liu Hao <108961479@qq.com>
2019-10-20 22:01:00 +0800

    ...

---
+++ MyTaLib/Sar.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyTaLib
+{
+    // 参考资料： https://school.stockcharts.com/doku.php?id=technical_indicators:parabolic_sar
+    public class Sar
+    {
+        private readonly double optAcc;
+        private readonly double optMaxAxx;
+        private readonly int maxCyclesNumber;
+
+        private List<Cycle> cycles;
+        private double ep;
+
+        public Sar(double optAcc, double optMaxAxx, int maxCyclesNumber)
+        {
+            this.optAcc = optAcc;
+            this.optMaxAxx = optMaxAxx;
+            this.maxCyclesNumber = maxCyclesNumber;
+        }
+
+        public void SetCycle0(double hi, double lo, double sar, double acc)
+        {
+            cycles = new List<Cycle>() { new Cycle() { Hi = hi, Lo = lo, Acc = acc, Sar = sar } };
+            ep = sar < lo ? hi : lo;
+        }
+
+        // return the newest sar value.
+        public double AddCycle(double hi, double lo)
+        {            
+            Cycle prevCycle = cycles.Last();
+            if (prevCycle.Sar < prevCycle.Lo)
+            {
+                ep = Math.Max(ep, prevCycle.Hi);
+                double sar = prevCycle.Sar + prevCycle.Acc * (ep - prevCycle.Sar);
+                if (sar > lo)
+                {
+                    // 上涨周期 =》 下跌周期
+                    sar = Math.Max(cycles.Max(o => o.Hi), hi);
+                    cycles.Add(new Cycle() { Hi = hi, Lo = lo, Acc = optAcc, Sar = sar });
+                }
+                else
+                {
+                    double acc = Math.Min(prevCycle.Acc + (hi > ep ? optAcc : 0.00d), optMaxAxx);
+                    cycles.Add(new Cycle() { Hi = hi, Lo = lo, Acc = acc, Sar = sar });
+                }
+            }
+            else
+            {
+                ep = Math.Min(ep, prevCycle.Lo);
+                double sar = prevCycle.Sar + prevCycle.Acc * (prevCycle.Lo - prevCycle.Sar);
+                if (sar < hi)
+                {
+                    // 下跌周期 =》 上涨周期
+                    sar = Math.Max(cycles.Min(o => o.Lo), lo);
+                    cycles.Add(new Cycle() { Hi = hi, Lo = lo, Acc = optAcc, Sar = sar });
+                }
+                else
+                {
+                    double acc = Math.Min(prevCycle.Acc + (lo > ep ? optAcc : 0.00d), optMaxAxx);
+                    cycles.Add(new Cycle() { Hi = hi, Lo = lo, Acc = acc, Sar = sar });
+                }
+            }
+
+            return cycles.Last().Sar;
+        }
+
+        private class Cycle
+        {
+            public double Hi;
+            public double Lo;
+
+            public double Acc;
+            public double Sar;
+        }
+    }
+}
--- StudyCSharp/Program.cs
+++ StudyCSharp/Program.cs
@@ -17,6 +17,7 @@ using Changes;
 using Changes.ViewModels;
 using Microsoft.Reactive.Testing;
 using Microsoft.Win32.SafeHandles;
+using MyTaLib;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using TicTacTec.TA.Library;
@@ -25,28 +26,41 @@ using static TicTacTec.TA.Library.Core;
 namespace StudyCSharp
 {
     public class Program
-    {   
+    {
         private static void Main(string[] args)
-        {   
-            CancellationTokenSource tokenSource = new CancellationTokenSource(); 
+        {
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
             OkexChanges changes = new OkexChanges();
 
-            //int granularity = 3600 * 24;
-            //List<Candle> candles = changes.GetCandlesAsync("BTC-USD-191227", granularity, null, null, tokenSource.Token).Result;
-            //Console.WriteLine(JsonConvert.SerializeObject(candles, Formatting.Indented));
+            int granularity = 3600 * 24;
+            List<Candle> candles = changes.GetCandlesAsync("BTC-USD-191227", granularity, null, null, tokenSource.Token).Result;
+            Console.WriteLine(JsonConvert.SerializeObject(candles, Formatting.Indented));
 
             //int count = candles.Count;
             //double[] hi = candles.Select(o => (double)o.HPrice).ToArray();
             //double[] lo = candles.Select(o => (double)o.LPrice).ToArray();
-            int count = 11;
-            double[] hi = { 8750.00, 8766.00, 8880.00, 8481.00, 8535.00, 8530.00, 8511.84, 8405.25, 8188.00, 8207.30, 8200.00 };
-            double[] lo = { 7751.00, 8490.00, 8325.00, 8260.00, 8329.00, 8177.00, 8283.79, 7995.00, 7962.00, 7854.00, 7919.65 };
-            double[] rt = new double[count];
+            int count0 = 10;
+            double[] hi0 = { 8766.00, 8880.00, 8481.00, 8535.00, 8530.00, 8511.84, 8405.25, 8188.00, 8207.30, 8200.00 };
+            double[] lo0 = { 8490.00, 8325.00, 8260.00, 8329.00, 8177.00, 8283.79, 7995.00, 7962.00, 7854.00, 7919.65 };
+
+            Sar sar = new Sar(0.02d, 0.2d, 4);
+            sar.SetCycle0(hi0[0], lo0[0], 7751.00d, 0.02d);
+            for (int i = 1; i < count0; ++i)
+            {
+                double value = sar.AddCycle(hi0[i], lo0[0]);
+                Console.WriteLine($"hi = {hi0[i]}, low = {lo0[i]}, sar = {value}");
+            }
+
+            //Ta-Lib test.
+            int count1 = 11;
+            double[] hi1 = { 8750.00, 8766.00, 8880.00, 8481.00, 8535.00, 8530.00, 8511.84, 8405.25, 8188.00, 8207.30, 8200.00 };
+            double[] lo1 = { 7751.00, 8490.00, 8325.00, 8260.00, 8329.00, 8177.00, 8283.79, 7995.00, 7962.00, 7854.00, 7919.65 };
+            double[] rt1 = new double[count1];
 
             int outBegIdx; int outNBElement;
             double optInAcceleration = 0.02d;
             double optInMaximum = 0.2d;
-            RetCode code = Core.Sar(0, count - 1, hi, lo, optInAcceleration, optInMaximum, out outBegIdx, out outNBElement, rt);
+            RetCode code = Core.Sar(0, count1 - 1, hi1, lo1, optInAcceleration, optInMaximum, out outBegIdx, out outNBElement, rt1);
         }
     }
 }
