f1e9eb8e66afc6a6c67e8204c4ab8f6e70f57a5f
Liu Hao <108961479@qq.com>
2019-10-23 22:00:09 +0800

    ...

--- DiInNetCore/Program.cs
+++ DiInNetCore/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
 /*
-    * 参考：https://andrewlock.net/using-dependency-injection-in-a-net-core-console-application/
-    */
+ * 参考：https://andrewlock.net/using-dependency-injection-in-a-net-core-console-application/
+ */
 namespace DiInNetCore
 {
     internal class Program
