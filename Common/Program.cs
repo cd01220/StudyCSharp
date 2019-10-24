a167f222d13a594ab935c1a0ef7be441afaa518f
Liu Hao <108961479@qq.com>
2019-10-24 22:58:05 +0800

    ...

--- Common/Program.cs
+++ Common/Program.cs
@@ -1,11 +1,25 @@
-﻿using System;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Common
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var serviceProvider = new ServiceCollection()
+            .AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddConsole();
+                loggingBuilder.AddDebug();
+            })
+            .BuildServiceProvider();
+
+            var logger = serviceProvider.GetService<ILoggerFactory>().CreateLogger<Program>();
+
+            logger.LogInformation("Starting application");
+            
             Console.WriteLine("Hello World!");
         }
     }
